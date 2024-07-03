858c16f70b64095490e946ffe4beae321a5bb432
Yousef Mohsen <yousefmohsen@outlook.dk>
2024-07-03 12:38:17 +0200

    matomo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,20 @@ export default function Document(props: Props) {
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+        <script
+        dangerouslySetInnerHTML={{
+          __html: `   var _paq = window._paq = window._paq || [];
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="https://stats.dbc.dk/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '43']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();`,
+        }}
+      />
       </Head>
       <body>
         <Main />
