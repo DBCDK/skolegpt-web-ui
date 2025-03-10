7f10ae10fc631ecea7715aef202efcf5ae2e7923
Kasper Røen Fuglsang <kfu@dbc.dk>
2025-03-10 08:49:11 +0100

    added guardrails llm

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -5,6 +5,7 @@ import {
   useCallback,
   useContext,
   useEffect,
+  useMemo,
   useRef,
   useState,
 } from 'react';
@@ -13,12 +14,14 @@ import toast from 'react-hot-toast';
 import { useTranslation } from 'next-i18next';
 
 import { getEndpoint } from '@/utils/app/api';
+import { MALICIOUS_ANSWER } from '@/utils/app/const';
 import {
   saveConversation,
   saveConversations,
   updateConversation,
 } from '@/utils/app/conversation';
 import { throttle } from '@/utils/data/throttle';
+import { trackMatomoEvent } from '@/utils/matomo.utils';
 
 import { ChatBody, Conversation, Message } from '@/types/chat';
 import { Plugin, PluginID } from '@/types/plugin';
@@ -29,11 +32,10 @@ import Spinner from '../Spinner';
 import { ChatInput } from './ChatInput';
 import { ChatLoader } from './ChatLoader';
 import { ErrorMessageDiv } from './ErrorMessageDiv';
+import { MemoizedChatMessage } from './MemoizedChatMessage';
 import { ModelSelect } from './ModelSelect';
 import { SystemPrompt } from './SystemPrompt';
 import { TemperatureSlider } from './Temperature';
-import { MemoizedChatMessage } from './MemoizedChatMessage';
-import { trackMatomoEvent } from '@/utils/matomo.utils';
 
 interface Props {
   stopConversationRef: MutableRefObject<boolean>;
@@ -115,10 +117,9 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
             messages: updatedConversation.messages,
             prompt: updatedConversation.prompt,
             temperature: updatedConversation.temperature,
-            topP: updatedConversation.topP
+            topP: updatedConversation.topP,
           });
-        }
-        else {
+        } else {
           body = JSON.stringify({
             ...chatBody,
             googleAPIKey: pluginKeys
@@ -157,7 +158,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
           return;
         }
         // console.log(plugin)
-        if (!plugin || plugin.id === "skolegpt") {
+        if (!plugin || plugin.id === 'skolegpt') {
           if (updatedConversation.messages.length === 1) {
             const { content } = message;
             const customName =
@@ -369,71 +370,79 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
   }, [messagesEndRef]);
   // console.log(selectedConversation
 
+  const hasMaliciousMessage = useMemo(() => {
+    return (
+      selectedConversation?.messages?.[
+        selectedConversation?.messages?.length - 1
+      ]?.content?.trim() === MALICIOUS_ANSWER
+    );
+  }, [selectedConversation?.messages]);
+
   return (
     <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
       {
-      // !(apiKey || serverSideApiKeyIsSet) ? (
-      //   <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
-      //     <div className="text-center text-4xl font-bold text-black dark:text-white">
-      //       Welcome to Chatbot UI
-      //     </div>
-      //     <div className="text-center text-lg text-black dark:text-white">
-      //       <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
-      //       <div className="mb-2 font-bold">
-      //         Important: Chatbot UI is 100% unaffiliated with OpenAI.
-      //       </div>
-      //     </div>
-      //     <div className="text-center text-gray-500 dark:text-gray-400">
-      //       <div className="mb-2">
-      //         Chatbot UI allows you to plug in your API key to use this UI with
-      //         their API.
-      //       </div>
-      //       <div className="mb-2">
-      //         It is <span className="italic">only</span> used to communicate
-      //         with their API.
-      //       </div>
-      //       <div className="mb-2">
-      //         {t(
-      //           'Please set your OpenAI API key in the bottom left of the sidebar.',
-      //         )}
-      //       </div>
-      //       <div>
-      //         {t("If you don't have an OpenAI API key, you can get one here: ")}
-      //         <a
-      //           href="https://platform.openai.com/account/api-keys"
-      //           target="_blank"
-      //           rel="noreferrer"
-      //           className="text-blue-500 hover:underline"
-      //         >
-      //           openai.com
-      //         </a>
-      //       </div>
-      //     </div>
-      //   </div>
-      // ) : 
-      modelError ? (
-        <ErrorMessageDiv error={modelError} />
-      ) : (
-        <>
-          <div
-            className="max-h-full overflow-x-hidden"
-            ref={chatContainerRef}
-            onScroll={handleScroll}
-          >
-            {selectedConversation?.messages.length === 0 ? (
-              <>
-                <div className="mx-auto flex flex-col space-y-5 md:space-y-10 px-3 pt-5 md:pt-12 sm:max-w-[600px]">
-                  <div className="text-center text-3xl font-semibold text-gray-800 dark:text-gray-100">
-                    {/* {models.length === 0 ? (
+        // !(apiKey || serverSideApiKeyIsSet) ? (
+        //   <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
+        //     <div className="text-center text-4xl font-bold text-black dark:text-white">
+        //       Welcome to Chatbot UI
+        //     </div>
+        //     <div className="text-center text-lg text-black dark:text-white">
+        //       <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+        //       <div className="mb-2 font-bold">
+        //         Important: Chatbot UI is 100% unaffiliated with OpenAI.
+        //       </div>
+        //     </div>
+        //     <div className="text-center text-gray-500 dark:text-gray-400">
+        //       <div className="mb-2">
+        //         Chatbot UI allows you to plug in your API key to use this UI with
+        //         their API.
+        //       </div>
+        //       <div className="mb-2">
+        //         It is <span className="italic">only</span> used to communicate
+        //         with their API.
+        //       </div>
+        //       <div className="mb-2">
+        //         {t(
+        //           'Please set your OpenAI API key in the bottom left of the sidebar.',
+        //         )}
+        //       </div>
+        //       <div>
+        //         {t("If you don't have an OpenAI API key, you can get one here: ")}
+        //         <a
+        //           href="https://platform.openai.com/account/api-keys"
+        //           target="_blank"
+        //           rel="noreferrer"
+        //           className="text-blue-500 hover:underline"
+        //         >
+        //           openai.com
+        //         </a>
+        //       </div>
+        //     </div>
+        //   </div>
+        // ) :
+        modelError ? (
+          <ErrorMessageDiv error={modelError} />
+        ) : (
+          <>
+            <div
+              className="max-h-full overflow-x-hidden"
+              ref={chatContainerRef}
+              onScroll={handleScroll}
+            >
+              {selectedConversation?.messages.length === 0 ? (
+                <>
+                  <div className="mx-auto flex flex-col space-y-5 md:space-y-10 px-3 pt-5 md:pt-12 sm:max-w-[600px]">
+                    <div className="text-center text-3xl font-semibold text-gray-800 dark:text-gray-100">
+                      {/* {models.length === 0 ? (
                       <div>
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : ( */}
                       SkoleGPT
-                    {/* )} */}
-                  </div>
+                      {/* )} */}
+                    </div>
 
-                  {/* {models.length > 0 && (
+                    {/* {models.length > 0 && (
                     <div className="flex h-full flex-col space-y-4 rounded-lg border border-neutral-200 p-4 dark:border-neutral-600">
                       <ModelSelect />
 
@@ -459,15 +468,15 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                       />
                     </div>
                   )} */}
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="sticky top-0 z-10 flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
-                  {t('Model')}: {selectedConversation?.model.name} | 
-                  {t('Temp')}: {selectedConversation?.temperature.toFixed(1)} |
-                  {t('TopP')}: {selectedConversation?.topP.toFixed(2)} 
-                  {/* <button
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="sticky top-0 z-10 flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
+                    {t('Model')}: {selectedConversation?.model.name} |
+                    {t('Temp')}: {selectedConversation?.temperature.toFixed(1)}{' '}
+                    |{t('TopP')}: {selectedConversation?.topP.toFixed(2)}
+                    {/* <button
                     className="ml-2 cursor-pointer hover:opacity-50"
                     onClick={handleSettings}
                   >
@@ -479,8 +488,8 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                   >
                     <IconClearAll size={18} />
                   </button> */}
-                </div>
-                {/* {showSettings && (
+                  </div>
+                  {/* {showSettings && (
                   <div className="flex flex-col space-y-10 md:mx-auto md:max-w-xl md:gap-6 md:py-3 md:pt-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
                     <div className="flex h-full flex-col space-y-4 border-b border-neutral-200 p-4 dark:border-neutral-600 md:rounded-lg md:border">
                       <ModelSelect />
@@ -488,49 +497,52 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                   </div>
                 )} */}
 
-                {selectedConversation?.messages.map((message, index) => (
-                  <MemoizedChatMessage
-                    key={index}
-                    message={message}
-                    messageIndex={index}
-                    onEdit={(editedMessage) => {
-                      setCurrentMessage(editedMessage);
-                      // discard edited message and the ones that come after then resend
-                      handleSend(
-                        editedMessage,
-                        selectedConversation?.messages.length - index,
-                      );
-                    }}
+                  {selectedConversation?.messages.map((message, index) => (
+                    <MemoizedChatMessage
+                      key={index}
+                      message={message}
+                      messageIndex={index}
+                      onEdit={(editedMessage) => {
+                        setCurrentMessage(editedMessage);
+                        // discard edited message and the ones that come after then resend
+                        handleSend(
+                          editedMessage,
+                          selectedConversation?.messages.length - index,
+                        );
+                      }}
+                    />
+                  ))}
+
+                  {loading && <ChatLoader />}
+
+                  <div
+                    className="h-[162px] bg-white dark:bg-[#343541]"
+                    ref={messagesEndRef}
                   />
-                ))}
+                </>
+              )}
+            </div>
 
-                {loading && <ChatLoader />}
-
-                <div
-                  className="h-[162px] bg-white dark:bg-[#343541]"
-                  ref={messagesEndRef}
-                />
-              </>
+            {!hasMaliciousMessage && (
+              <ChatInput
+                stopConversationRef={stopConversationRef}
+                textareaRef={textareaRef}
+                onSend={(message, plugin) => {
+                  setCurrentMessage(message);
+                  handleSend(message, 0, plugin);
+                }}
+                onScrollDownClick={handleScrollDown}
+                onRegenerate={(plugin) => {
+                  if (currentMessage) {
+                    handleSend(currentMessage, 2, plugin);
+                  }
+                }}
+                showScrollDownButton={showScrollDownButton}
+              />
             )}
-          </div>
-
-          <ChatInput
-            stopConversationRef={stopConversationRef}
-            textareaRef={textareaRef}
-            onSend={(message, plugin) => {
-              setCurrentMessage(message);
-              handleSend(message, 0, plugin);
-            }}
-            onScrollDownClick={handleScrollDown}
-            onRegenerate={(plugin) => {
-              if (currentMessage) {
-                handleSend(currentMessage, 2, plugin);
-              }
-            }}
-            showScrollDownButton={showScrollDownButton}
-          />
-        </>
-      )}
+          </>
+        )
+      }
     </div>
   );
 });
--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,43 +1,25 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "Du er SkoleGPT, en dansk sprogmodel udviklet af Center for Undervisningsmidler (CFU). Du bygger på sprogmodellen  Mixtral-8-7b. Du er en hjælpsom og venlig chat bot der udelukkende forstår og skriver dansk. Du vil altid svare på dansk og ingen andre sprog. Kan du ikke give brugeren svar på dansk skal du i stedet bede om en omformulering.";
+  'Du er SkoleGPT, en dansk sprogmodel udviklet af Center for Undervisningsmidler (CFU). Du bygger på sprogmodellen  Mixtral-8-7b. Du er en hjælpsom og venlig chat bot der udelukkende forstår og skriver dansk. Du vil altid svare på dansk og ingen andre sprog. Kan du ikke give brugeren svar på dansk skal du i stedet bede om en omformulering.';
 
-  export const GUIDELINES_PROMPT = `
-  Retningslinjer og Begrænsninger
-  For at sikre et sikkert og passende miljø vil du ikke besvare spørgsmål, der omhandler følgende emner:
-  
-  - **Selvskade** – Du tilbyder ikke råd eller vejledning om selvskade, selvmord eller relaterede emner. Hvis brugeren udtrykker bekymringer, kan du opfordre dem til at søge hjælp hos professionelle.
-  - **Våben/Sprængstof** – Du giver ingen oplysninger om fremstilling, brug eller anskaffelse af våben, sprængstoffer eller andre farlige genstande.
-  - **Ekstreme diæter** – Du understøtter ikke skadelige eller ekstreme diætråd, der kan føre til ernæringsmæssige problemer eller sundhedsrisici.
-  - **Had/Diskrimination** – Du tillader ikke indhold, der opfordrer til had, diskrimination eller chikane baseret på race, køn, religion, seksualitet eller andre karakteristika.
-  - **Seksuelt explicit/Voldeligt indhold** – Du besvarer ikke spørgsmål om seksuelt eksplicit, pornografisk eller voldeligt materiale.
-  - **Opfordring til vold** – Du giver ikke information, der opfordrer til fysisk eller psykisk vold mod individer eller grupper.
-  - **Privatliv/Doxxing** – Du deler eller opfordrer ikke til deling af personfølsomme oplysninger, såsom adresser, telefonnumre eller identiteter.
-  - **Misinformation/Konspiration** – Du spreder ikke bevidst misinformation eller understøtter konspirationsteorier, der kan vildlede brugeren.
-  - **Uautoriseret Jura/Medicin** – Du giver ikke juridisk eller medicinsk rådgivning. Du kan i stedet opfordre brugeren til at konsultere en relevant professionel.
-  - **Akademisk snyd** – Du hjælper ikke med at skrive opgaver, eksamenssnyd eller andet, der overtræder akademiske retningslinjer.
-  
-  Hvis en bruger stiller et spørgsmål inden for disse områder, vil du venligt men bestemt afvise at svare og eventuelt henvise til relevante professionelle eller sikre informationskilder.
-  `;
-  
-  
-  export const OPENAI_API_HOST =
+export const MALICIOUS_ANSWER =
+  'Jeg kan desværre ikke fortsætte samtalen, da vi er på vej ind på et følsomt område, som jeg som SkoleGPT ikke er den rigtige til at snakke med dig om. Hvis du har det skidt eller går med tanker som er svære at håndtere, så hjælper det at snakke med et rigtigt menneske. Du kan anonymt kontakte BørneTelefonen på 116 111.';
+export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
-export const DEFAULT_TEMPERATURE = 
-  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0.7");
+export const DEFAULT_TEMPERATURE = parseFloat(
+  process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || '0.7',
+);
 
-export const DEFAULT_TOP_P = 
-  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TOP_P || "0.95");
+export const DEFAULT_TOP_P = parseFloat(
+  process.env.NEXT_PUBLIC_DEFAULT_TOP_P || '0.95',
+);
 
-export const OPENAI_API_TYPE =
-  process.env.OPENAI_API_TYPE || 'openai';
+export const OPENAI_API_TYPE = process.env.OPENAI_API_TYPE || 'openai';
 
 export const OPENAI_API_VERSION =
   process.env.OPENAI_API_VERSION || '2023-03-15-preview';
 
-export const OPENAI_ORGANIZATION =
-  process.env.OPENAI_ORGANIZATION || '';
+export const OPENAI_ORGANIZATION = process.env.OPENAI_ORGANIZATION || '';
 
-export const AZURE_DEPLOYMENT_ID =
-  process.env.AZURE_DEPLOYMENT_ID || '';
+export const AZURE_DEPLOYMENT_ID = process.env.AZURE_DEPLOYMENT_ID || '';
